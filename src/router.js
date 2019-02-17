260018cb5b156283f5b4848cb3e13811f422ed8e
Vedat Murat <vedat.murat@primeholding.com>
2019-02-17 21:16:08 +0200

    Add global navigation and footer components

--- src/router.js
+++ src/router.js
@@ -1,11 +1,9 @@
 import Vue from 'vue';
 import Router from "vue-router";
-import Navigation from './components/navigation/Navigation';
 import People from "./components/people/People";
 import About from "./components/about/About";
 import Planets from "./components/planets/Planets";
 import Starships from "./components/starships/Starships";
-import Footer from './components/footer/Footer';
 
 Vue.use(Router);
 
@@ -19,33 +17,25 @@ export default new Router({
             path: '/people',
             name: 'people',
             components: {
-                navigation: Navigation,
-                default: People,
-                footer: Footer,
+                default: People
             }    
         },
         {
             path: '/about',
             name: 'about',
             components: {
-                navigation: Navigation,
-                default: About,
-                footer: Footer,
+                default: About
             } 
         },
         {
             path: '/planets',
             name: 'planets',
-            navigation: Navigation,
-                default: Planets,
-                footer: Footer,
+                default: Planets
         },
         {
             path: '/starships',
             name: 'starships',
-            navigation: Navigation,
-                default: Starships,
-                footer: Footer,
+                default: Starships
         }
     ]
 });
